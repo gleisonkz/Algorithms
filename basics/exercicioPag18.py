4a6ec50038bcfbb665d0bc556ccc22b3645403fc
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-08-19 22:16:38 -0300

    change to full english

--- basics/exercicioPag18.py
+++ basics/exercicioPag18.py
@@ -8,4 +8,4 @@ fruit4 = {"name": getFruitName(), "value": fruit3["value"] * 3}
 fruit5 = {"name": getFruitName(), "value": fruit4["value"] / 2}
 fruits = [fruit1, fruit2, fruit3, fruit4, fruit5]
 for fruit in fruits:
-    print(f" A fruta {fruit['name']} custa {fruit['value']} ")
+    print(f" Fruit {fruit['name']} costs {fruit['value']} R$")
