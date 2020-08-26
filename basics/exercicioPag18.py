1fe5469a5444dd54464e14eea9959d6b72e8ad81
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-08-26 19:16:15 -0300

    enhance implementation

--- basics/exercicioPag18.py
+++ basics/exercicioPag18.py
@@ -1,11 +1,12 @@
 def getFruitName():
     return input("Write the fruit name: ")
 
-fruit1 = {"name": getFruitName(), "value": 1.00}
-fruit2 = {"name": getFruitName(), "value": fruit1["value"] * 2}
-fruit3 = {"name": getFruitName(), "value": fruit1["value"] / 2}
-fruit4 = {"name": getFruitName(), "value": fruit3["value"] * 3}
-fruit5 = {"name": getFruitName(), "value": fruit4["value"] / 2}
-fruits = [fruit1, fruit2, fruit3, fruit4, fruit5]
+fruits = [
+    {"name": getFruitName(), "value": 1.00},
+    {"name": getFruitName(), "value": 1.00 * 2},
+    {"name": getFruitName(), "value": 1.00 / 2},
+    {"name": getFruitName(), "value": (1.00 / 2) * 3},
+    {"name": getFruitName(), "value": ((1.00 / 2) * 3) / 2}]
+
 for fruit in fruits:
     print(f" Fruit {fruit['name']} costs {fruit['value']} R$")
