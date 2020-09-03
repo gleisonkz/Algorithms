a72e48e208ea251cd27a4ffc3b4eeb3478d9a5de
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-03 07:59:29 -0300

    rename

--- basics/typestest.py
+++
@@ -1,6 +0,0 @@
-items = [{
-    "key": input("Item name: "),
-    "value": int(input("Quantity: "))}
-    for item in range(5)]
-for index, item in enumerate(items):
-    print(f"Item {index + 1} | Value: {item['key']} | Qtd: {item['value']}")
