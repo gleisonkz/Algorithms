9770bf69bfcc315b0435f2b31e758b2b0ad6083e
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-21 07:32:36 -0300

    feat: resolution of two first challenges on CodeSignal

---
+++ type-script/CodeSignal/1 - sum.ts
@@ -0,0 +1,14 @@
+/*
+TODO - Write a function that returns the sum of two numbers.
+
+Example
+For param1 = 1 and param2 = 2, the output should be
+add(param1, param2) = 3. 
+*/
+
+function add(number1: number, number2: number): number {
+    return number1 + number2
+}
+
+const sum = add(1, 2)
+console.log(sum)
