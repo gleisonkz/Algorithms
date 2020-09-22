a20afb3b21136e3d8a34624b40a5cf9d50060d44
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-09-22 13:12:37 -0300

    refactor: creation of function divisibleBy and change to full english.

--- typescript/Exercism/leap/leap.ts
+++ typescript/Exercism/leap/leap.ts
@@ -1,36 +1,6 @@
-function isLeapYear(ano: number) {
-
-    const divididoPor4 = ano % 4 === 0;
-    const divididoPor100 = ano % 100 === 0;
-    const divididoPor400 = ano % 400 === 0;
-
-
-
-
-    return divididoPor4 && !divididoPor100 || divididoPor400;
-
-
-    //return  divididoPor400 || !divididoPor100 && divididoPor4;
-
-
-
-
-
-    //return divididoPor4 && !(divididoPor400 !== divididoPor100)
-
-    // let check = false;
-    // if (ano % 4 === 0 ) {
-    //     check = true;
-    //     if(ano % 100 === 0 && ano % 400 === 0) {
-    //         check = true
-    //     } else {
-    //         check = false
-    //     }
-    // }
-    // return check
-
+function isLeapYear(year: number) {
+  const divisibleBy = (operand: number) => year % operand === 0;
+  return (divisibleBy(4) && !divisibleBy(100)) || divisibleBy(400);
 }
 
-
-
-export default isLeapYear
+export default isLeapYear;
