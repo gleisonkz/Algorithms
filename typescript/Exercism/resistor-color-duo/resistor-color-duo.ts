2fb0509e9d912fd2b35b312df3137beae9cbea17
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-24 20:39:13 -0300

    refactor: extract local variable to private property

--- typescript/Exercism/resistor-color-duo/resistor-color-duo.ts
+++ typescript/Exercism/resistor-color-duo/resistor-color-duo.ts
@@ -8,26 +8,25 @@ export class ResistorColor {
       },
     },
   ];
+  private keyValuePairColors: { [key: string]: number } = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
   constructor(colors: string[]) {
     this.colors = colors;
     this.validations.find((validation) => validation.key())?.value();
   }
   value = (): number => {
-    const keyValuePairColors: { [key: string]: number } = {
-      black: 0,
-      brown: 1,
-      red: 2,
-      orange: 3,
-      yellow: 4,
-      green: 5,
-      blue: 6,
-      violet: 7,
-      grey: 8,
-      white: 9,
-    };
-
     return +this.colors
-      .map((c) => keyValuePairColors[c])
+      .map((c) => this.keyValuePairColors[c])
       .slice(0, 2)
       .join("");
   };
